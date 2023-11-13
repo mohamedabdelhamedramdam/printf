bad7d9b2a6b0a53353ac60d9fcad3df4f6d27cba
MOhmaed-Abdelhamed <mohamadabdelhamed14@gmail.com>
2023-11-13 05:35:01 -0800

    added putchar_str.c file

---
+++ putchar_str.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_str - prints a string passed as an arg
+ * @args: a variable argument list with  a string pointer
+ * Return: the number of characters printed
+*/
+
+int print_str(va_list args)
+{
+	char *str = va_arg(args, char *);
+	int count;
+
+	count = 0;
+	while (*str != '\0')
+	{
+		count += _putchar(*str);
+		str++;
+	}
+
+	return (count);
+}
