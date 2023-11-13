f7a99f571bba6295b7eda6f18eada669967ce162
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2023-11-13 16:44:02 +0200

    add get_format_function.c

---
+++ get_format_function.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * get_format_func - get the proper format function
+ * @dir: the directive
+ *
+ * Return: number of characters printed
+*/
+
+int (*get_format_func(char dir))(va_list args)
+{
+	f_o formats_functions[] = {
+		{'c', print_char},
+		{'d', print_int},
+		{'i', print_int},
+		{'s', print_str},
+		{'%', print_percent},
+		{0, NULL}
+	};
+	int i;
+
+	i = 0;
+	while (formats_functions[i].abb != 0)
+	{
+		if (dir == formats_functions[i].abb)
+			return (formats_functions[i].f);
+		i++;
+	}
+
+	return (NULL);
+}
