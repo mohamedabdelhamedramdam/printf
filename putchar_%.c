d7539b135f807615d1787310400edffb00425c7e
ahmedsaadkader <ahmed.saad.kader@gmail.com>
2023-11-13 17:56:46 +0200

    betty

--- putchar_%.c
+++ putchar_%.c
@@ -2,10 +2,12 @@
 
 /**
  * print_percent - print %
+ * @args: a variable argument list
+ *
  * Return: 1
 */
 
 int print_percent(va_list args)
 {
-    return (_putchar(va_arg(args, int)));
+	return (_putchar(va_arg(args, int)));
 }
